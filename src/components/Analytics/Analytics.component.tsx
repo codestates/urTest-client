0a27e1648f9132cf7260c42d76cc4516056acb61
afashs <dev.afashs@gmail.com>
2021-04-15 11:40:46 +0900

    feature_modify_#65: Added piechart and img result

--- src/components/Analytics/Analytics.component.tsx
+++ src/components/Analytics/Analytics.component.tsx
@@ -5,6 +5,7 @@ import CommentItem from "../Comment/CommentItem.component";
 import SweetAlert from "react-bootstrap-sweetalert";
 import BootstrapTable from "react-bootstrap-table-next";
 import { Col, Row, Image, Jumbotron, ProgressBar } from "react-bootstrap";
+import { PieChart } from "react-minimal-pie-chart";
 
 const GET_CONTENT = gql`
   query getContent($id: Int!) {
@@ -66,6 +67,8 @@ const ADD_COMMENT = gql`
 
 const Analytics = (props: any) => {
   const [sweetAlertShow, setSweetAlertShow] = useState(false);
+  const [imgSweetAlertSrc, setImgSweetAlertSrc] = useState("");
+  const [imgSweetAlertShow, setImgSweetAlertShow] = useState(false);
   const [type, setType] = useState("" as any);
   const [imgs, setImgs] = useState([] as any[]);
   const [winCountTotal, setWinCountTotal] = useState(0 as any);
@@ -153,7 +156,19 @@ const Analytics = (props: any) => {
   });
 
   const imageFormatter = (cell: any) => {
-    return <Image src={`${cell}`} thumbnail />;
+    return (
+      <Image
+        onClick={(e) => {
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          setImgSweetAlertSrc(e.target.src);
+          setImgSweetAlertShow(true);
+        }}
+        className="thumb"
+        src={`${cell}`}
+        thumbnail
+      />
+    );
   };
   const questionFomatter = (cell: any, row: any) => {
     const winTotal = row.answer1Count + row.answer2Count;
@@ -162,6 +177,28 @@ const Analytics = (props: any) => {
         <div>{cell}</div>
         <div>투표 수 : {winTotal}</div>
       </>
+    );
+  };
+
+  const answerRatingFormatter = (cell: any, row: any) => {
+    const winTotal = row.answer1Count + row.answer2Count;
+    const answer1Rate = Math.ceil((row.answer1Count / winTotal) * 100);
+    const answer2Rate = Math.ceil((row.answer2Count / winTotal) * 100);
+    return (
+      <PieChart
+        data={[
+          { title: row.answer1, value: answer1Rate, color: "#6bb2ff" },
+          { title: row.answer2, value: answer2Rate, color: "#f74a5b" },
+        ]}
+        animate
+        label={({ dataEntry }) => dataEntry.value + "%"}
+        // labelPosition={100 - 60 / 2}
+        labelStyle={{
+          fill: "#fff",
+          opacity: 0.75,
+          pointerEvents: "none",
+        }}
+      />
     );
   };
   const answer1RatingFormatter = (cell: any, row: any) => {
@@ -173,17 +210,16 @@ const Analytics = (props: any) => {
       return (
         <>
           <p>{cell}</p>
-          {now < 50 ? (
-            <ProgressBar animated now={now} label={`${now}%`} />
-          ) : (
+          {/* {now < 50 ? ( */}
+          <ProgressBar animated now={now} label={`${now}%`} />
+          {/* ) : (
             <ProgressBar
               animated
               now={now}
               label={`${now}%`}
               variant="danger"
             />
-          )}
-
+          )} */}
           <p>[ {row.answer1Count} ]</p>
         </>
       );
@@ -198,16 +234,11 @@ const Analytics = (props: any) => {
       return (
         <>
           <p>{cell}</p>
-          {now < 50 ? (
+          {/* {now < 50 ? (
             <ProgressBar animated now={now} label={`${now}%`} />
-          ) : (
-            <ProgressBar
-              animated
-              now={now}
-              label={`${now}%`}
-              variant="danger"
-            />
-          )}
+          ) : ( */}
+          <ProgressBar animated now={now} label={`${now}%`} variant="danger" />
+          {/* )} */}
 
           <p>[ {row.answer2Count} ]</p>
         </>
@@ -259,14 +290,14 @@ const Analytics = (props: any) => {
   };
 
   const textColumns = [
-    {
-      dataField: "id",
-      text: "번호",
-      headerStyle: {
-        width: "10%",
-      },
-      headerAlign: "center",
-    },
+    // {
+    //   dataField: "id",
+    //   text: "번호",
+    //   headerStyle: {
+    //     width: "10%",
+    //   },
+    //   headerAlign: "center",
+    // },
     {
       dataField: "question",
       text: "질문",
@@ -291,17 +322,25 @@ const Analytics = (props: any) => {
       },
       formatter: answer2RatingFormatter,
     },
+    {
+      dataField: "answer",
+      text: "승률",
+      headerStyle: {
+        width: "30%",
+      },
+      formatter: answerRatingFormatter,
+    },
   ];
 
   const imgColumns = [
-    {
-      dataField: "id",
-      text: "번호",
-      headerStyle: {
-        width: "10%",
-      },
-      headerAlign: "center",
-    },
+    // {
+    //   dataField: "id",
+    //   text: "번호",
+    //   headerStyle: {
+    //     width: "10%",
+    //   },
+    //   headerAlign: "center",
+    // },
     {
       dataField: "photoUrl",
       text: "사진",
@@ -324,7 +363,7 @@ const Analytics = (props: any) => {
       text: "승률 [ 우승 / 전체 플레이 ]",
       sort: true,
       headerStyle: {
-        width: "40%",
+        width: "50%",
       },
       formatter: ratingFormatter,
     },
@@ -354,7 +393,17 @@ const Analytics = (props: any) => {
             <p>{contentDesc}</p>
           </Jumbotron>
           {type === "imggame" ? (
-            <BootstrapTable keyField="id" data={imgs} columns={imgColumns} />
+            <BootstrapTable
+              keyField="id"
+              data={imgs}
+              columns={imgColumns}
+              defaultSorted={[
+                {
+                  dataField: "winCount",
+                  order: "desc",
+                },
+              ]}
+            />
           ) : (
             <BootstrapTable
               keyField="id"
@@ -406,6 +455,19 @@ const Analytics = (props: any) => {
               setSweetAlertShow(false);
             }}
           />
+          <SweetAlert
+            title=""
+            show={imgSweetAlertShow}
+            showConfirm={false}
+            onConfirm={() => {
+              setImgSweetAlertShow(false);
+            }}
+            onCancel={() => {
+              setImgSweetAlertShow(false);
+            }}
+          >
+            <Image src={`${imgSweetAlertSrc}`} className="w-100" />
+          </SweetAlert>
         </Col>
       </Row>
     </>
